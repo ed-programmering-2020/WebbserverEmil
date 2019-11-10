b434212342f2d2cd55efe827f5a349c0e421e45c
Neobyte01 <neobyte01@hotmail.com>
2019-11-10 12:51:13 +0100

    Convert into Orpose

--- Orpose/wsgi.py
+++ Orpose/wsgi.py
@@ -1,7 +1,9 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+from Orpose.settings.base import BASE_DIR
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Orpose.settings')
 
 application = get_wsgi_application()
