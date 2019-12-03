bc68990d608eb282c2727119a8b572f977ce0de0
Neobyte01 <neobyte01@hotmail.com>
2019-12-03 16:42:16 +0100

    product combiner

--- Orpose/urls.py
+++ Orpose/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,7 +7,7 @@ from .views import FrontendAppView
 
 
 urlpatterns = [
-    path(r'^.*/', FrontendAppView.as_view()),
+    re_path(r'^.*/', FrontendAppView.as_view()),
     path("admin/", admin.site.urls),
     path("api/", include("products.urls")),
     path("api/scraping/", include("scraping.urls")),
