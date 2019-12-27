5bb7fa8a7e22ea3fef7988445ff7864ced6aef64
Neobyte01 <neobyte01@hotmail.com>
2019-12-27 23:48:03 +0100

    integrate react router with django urls

--- Orpose/settings/base.py
+++ Orpose/settings/base.py
@@ -62,7 +62,8 @@ ROOT_URLCONF = 'Orpose.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, "static")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
