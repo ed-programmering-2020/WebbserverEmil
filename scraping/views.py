c0248b33e5a85117be2cb6fee782e573db63981b
Neobyte01 <neobyte01@hotmail.com>
2020-01-04 13:18:25 +0100

    integrate react router with django urls

--- scraping/views.py
+++ scraping/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -33,6 +33,9 @@ class ProductsAPI(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.data.get("products"))
         files = request.FILES
+
+        print(data)
+        print(files)
 
         Combiner(data, files)
         return Response({})
