7d696ae42f1c727b357b94b98a800d7e7228d56d
Neobyte01 <neobyte01@hotmail.com>
2020-01-04 13:16:32 +0100

    integrate react router with django urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegistrationAPI, LoginAPI, UserAPI, LogoutAPI
+from .views import RegistrationAPI, LoginAPI, UserAPI, LogoutAPI, TokenAPI
 
 
 urlpatterns = [
     path("register/", RegistrationAPI.as_view()),
     path("login/", LoginAPI.as_view()),
     path("logout/", LogoutAPI.as_view()),
-    path("user/", UserAPI.as_view())
+    path("user/", UserAPI.as_view()),
+    path("token/", TokenAPI.as_view())
 ]
