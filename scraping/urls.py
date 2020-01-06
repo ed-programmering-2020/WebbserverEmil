c72d034c3e71f4ee20dd908edc33a7c6df11cc31
Neobyte01 <neobyte01@hotmail.com>
2020-01-06 14:23:51 +0100

    crawler solo mode api url

--- scraping/urls.py
+++ scraping/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WebsitesAPI, ProductsAPI
+from .views import WebsitesAPI, ProductsAPI, AllWebsitesAPI
 
 urlpatterns = [
     path("websites/", WebsitesAPI.as_view()),
     path("products/", ProductsAPI.as_view()),
+    path("all-websites/", AllWebsitesAPI.as_view())
 ]
