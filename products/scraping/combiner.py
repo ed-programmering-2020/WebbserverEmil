663f2198c4771ef72dcdbe39245d65b0bae01fb7
Neobyte01 <neobyte01@hotmail.com>
2020-01-22 21:41:05 +0100

    fix

--- products/scraping/combiner.py
+++ products/scraping/combiner.py
@@ -2,8 +2,7 @@ from operator import itemgetter
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from difflib import SequenceMatcher
-from products.models import MetaProduct, Product
-from scraping.models import Website
+from products.models import MetaProduct, Product, Website
 import string
 
 
