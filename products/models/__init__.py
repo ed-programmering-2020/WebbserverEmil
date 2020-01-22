730e197ffee96ffd62a0510f28ba26f2ea042f69
Neobyte01 <neobyte01@hotmail.com>
2020-01-22 21:57:09 +0100

    fix

--- products/models/__init__.py
+++ products/models/__init__.py
@@ -1,2 +1,2 @@
-from .products import Website, Country, Product, Price, MetaProduct, SpecValue, SpecKey, SpecGroup
+from .products import Website, Country, Product, Price, MetaProduct, SpecValue, SpecKey, SpecGroup, get_file_path
 from .categories import MetaCategory, Category
