e79f3e8fee2e021a311079dd3eef66c3d1bf4311
neobyte01 <neobyte01@gmail.com>
2020-01-31 14:04:07 +0100

    Removal of manufacturer

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, MetaProduct, SpecValue, SpecKey, SpecGroup, Price, Website, Category, MetaCategory
+from .models import Product, MetaProduct, SpecValue, SpecKey, Price, Website, Category, MetaCategory #, SpecGroup
 
 
 class ProductAdmin(admin.ModelAdmin):
