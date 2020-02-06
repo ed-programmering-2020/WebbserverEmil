31972a1a06ec0149c26a03a7d55827fd1776c28f
Neobyte01 <neobyte01@hotmail.com>
2020-02-06 21:37:15 +0100

    fix

--- content/views.py
+++ content/views.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Feedback
 
@@ -9,3 +10,4 @@ class FeedbackAPI(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         message = request.POST["message"]
         Feedback.objects.create(message=message)
+        return Response({"message": "success"})
