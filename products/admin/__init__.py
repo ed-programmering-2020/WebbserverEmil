8a4b67dba0065650d7eebc8cabcc149bbb8b4225
Neobyte01 <neobyte01@hotmail.com>
2020-02-20 16:43:23 +0100

    Fix when metaproduct get price returns None in product combiner

---
+++ products/admin/__init__.py
@@ -0,0 +1,10 @@
+from .categories import Category
+from .spec_groups import SpecGroup, Benchmark
+from .products import \
+    ProductAdmin, \
+    MetaProductAdmin, \
+    SpecValueAdmin,\
+    SpecKeyAdmin,\
+    PriceAdmin, \
+    WebsiteAdmin,\
+    MetaCategoryAdmin
