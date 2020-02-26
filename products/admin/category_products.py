0bdb05ae99f82b922c46df71a367f6c322baf716
Neobyte01 <neobyte01@hotmail.com>
2020-02-26 14:14:35 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/admin/category_products.py
+++ products/admin/category_products.py
@@ -21,7 +21,7 @@ class BaseCategoryProductAdmin(admin.ModelAdmin):
 
 @admin.register(Laptop)
 class LaptopAdmin(BaseCategoryProductAdmin):
-    super(BaseCategoryProductAdmin).fieldsets.append(
+    BaseCategoryProductAdmin.fieldsets.append(
         ("Specifications", {
             "fields": [
                 "battery_time",
