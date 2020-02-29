591624bd9daa84665c84ebfdc551d0a7cb280cd4
Neobyte01 <neobyte01@hotmail.com>
2020-02-29 10:49:55 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/management/commands/updatecategoryproducts.py
+++ products/management/commands/updatecategoryproducts.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     help = "Update all category products"
 
     def handle(self, *args, **kwargs):
-        for category_product in BaseCategoryProduct.objects.all().iterator():
+        for category_product in BaseCategoryProduct.objects.filter(is_active=True).iterator():
             category_product.update()
 
         self.stdout.write(self.style.SUCCESS("Successfully updated category products"))
