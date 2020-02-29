38fd8b7f5ed99c86063a0dafdfb2980f0ba4dc3b
Neobyte01 <neobyte01@hotmail.com>
2020-02-29 12:44:35 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/models/specifications/base/specification.py
+++ products/models/specifications/base/specification.py
@@ -117,7 +117,7 @@ class BaseSpecification(PolymorphicModel):
                 specification_type = SpecificationType.objects.create(name=cls.__name__)
 
             # Create dummy category product
-            cls.create(specification=specification_type)
+            cls.create(specification_type=specification_type)
 
     @staticmethod
     def get_specification_instances(product_data):
