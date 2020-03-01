fd3f2db5dca3ab702842f3be523efae1f32913ba
Neobyte01 <neobyte01@hotmail.com>
2020-03-01 16:35:07 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/models/category_products/base.py
+++ products/models/category_products/base.py
@@ -252,6 +252,7 @@ class BaseCategoryProduct(PolymorphicModel):
                 for specification in specification_instances:
                     specification_attribute_name = specification.get_attribute_like_name()
                     exec("self.{}_id = {}".format(specification_attribute_name, specification.id))
+                    print(eval("self.{}_id".format(specification_attribute_name)))
 
         # Update the rest of the category product
         self.update_name(data["names"])
