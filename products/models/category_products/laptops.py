8e27639ef2631897730bf98425b0d0391afe6ed6
Neobyte01 <neobyte01@hotmail.com>
2020-03-01 14:32:42 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/models/category_products/laptops.py
+++ products/models/category_products/laptops.py
@@ -60,10 +60,13 @@ class Laptop(BaseCategoryProduct):
         size = settings.get("size", None)
         if size is not None:
             size_dict = json.loads(size)
+            print(size_dict)
+            min_price = size_dict["min"]
+            max_price = size_dict["max"]
 
             filtered_laptops = []
             for laptop in laptops:
-                if size_dict["min"] < laptop.screen_size < size_dict["max"]:
+                if min_price < laptop.screen_size < max_price:
                     filtered_laptops.append(laptop)
 
             laptops = filtered_laptops
