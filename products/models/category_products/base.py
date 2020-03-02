aa6fb83c30b93feb5b199e114fc8099b8dfd6f20
Neobyte01 <neobyte01@hotmail.com>
2020-03-02 22:11:58 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/models/category_products/base.py
+++ products/models/category_products/base.py
@@ -289,51 +289,54 @@ class BaseCategoryProduct(PolymorphicModel):
         self.save()
 
     def update_name(self, names):
-        words = {}
-        for name in names:
-            split_words = name.split(" ")
+        if len(names) >= 3:
+            words = {}
+            for name in names:
+                split_words = name.split(" ")
 
-            for other_name in names:
-                if other_name != name:
-                    other_split_words = other_name.split(" ")
+                for other_name in names:
+                    if other_name != name:
+                        other_split_words = other_name.split(" ")
 
-                    for pos, word in enumerate(other_split_words):
-                        for split_word in split_words:
-                            if SequenceMatcher(None, split_word, word).ratio() >= 0.9 and pos >= 0:
-                                if word in words:
-                                    words[word].append(pos)
-                                else:
-                                    words[word] = [pos]
+                        for pos, word in enumerate(other_split_words):
+                            for split_word in split_words:
+                                if SequenceMatcher(None, split_word, word).ratio() >= 0.9 and pos >= 0:
+                                    if word in words:
+                                        words[word].append(pos)
+                                    else:
+                                        words[word] = [pos]
 
-        words_copy = words.copy()
-        removed_words = []
-        for word in words_copy:
-            if word not in removed_words:
-                for other_word in words_copy:
-                    if word != other_word:
-                        if SequenceMatcher(None, other_word, word).ratio() >= 0.9:
-                            length = len(words[word])
-                            other_word = words.get(other_word, None)
-                            if not other_word:
-                                other_length = len(other_word)
+            words_copy = words.copy()
+            removed_words = []
+            for word in words_copy:
+                if word not in removed_words:
+                    for other_word in words_copy:
+                        if word != other_word:
+                            if SequenceMatcher(None, other_word, word).ratio() >= 0.9:
+                                length = len(words[word])
+                                other_word = words.get(other_word, None)
+                                if not other_word:
+                                    other_length = len(other_word)
 
-                                if length >= other_length:
-                                    words.pop(other_word, None)
-                                    removed_words.append(other_word)
-                                else:
-                                    words.pop(word, None)
-                                    removed_words.append(word)
+                                    if length >= other_length:
+                                        words.pop(other_word, None)
+                                        removed_words.append(other_word)
+                                    else:
+                                        words.pop(word, None)
+                                        removed_words.append(word)
 
-        calc_words = {word: (sum(p) / len(p)) for (word, p) in words.items()}
-        sorted_words = sorted(calc_words.items(), key=lambda kv: kv[1])
-        name = ""
-        for word, pos in sorted_words:
-            if len(name) > 0:
-                name += " " + word
-            else:
-                name = word
+            calc_words = {word: (sum(p) / len(p)) for (word, p) in words.items()}
+            sorted_words = sorted(calc_words.items(), key=lambda kv: kv[1])
+            name = ""
+            for word, pos in sorted_words:
+                if len(name) > 0:
+                    name += " " + word
+                else:
+                    name = word
 
-        if name == "" and names != []:
+            if name == "" and names != []:
+                name = names[0]
+        else:
             name = names[0]
 
         self.name = name
