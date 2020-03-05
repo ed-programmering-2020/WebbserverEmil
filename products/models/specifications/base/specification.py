ad9501b40692a8a561d3c6285d0eeb6529e7f40e
Neobyte01 <neobyte01@hotmail.com>
2020-03-05 08:28:24 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/models/specifications/base/specification.py
+++ products/models/specifications/base/specification.py
@@ -156,9 +156,10 @@ class BaseSpecification(PolymorphicModel):
 
                         # Find existing specification instance, else create a new specification
                         for spec in specification_model.objects.all():
+                            print("compare", spec)
                             if spec.is_equal(processed_spec):
                                 specification = spec
-                                print(spec)
+                                print("equal", spec)
                                 break
                         else:
                             specification = specification_model.polymorphic_create(specification_type=specification_type)
