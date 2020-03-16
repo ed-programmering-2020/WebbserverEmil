c98b7e080a03503e84e35c63f1f9a1627ab5856f
Neobyte01 <neobyte01@hotmail.com>
2020-03-16 15:23:05 +0100

    Fix when metaproduct get price returns None in product combiner

--- content/admin.py
+++ content/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Feedback
+from .models import Feedback, CategorySurveyAnswer
 
 
+@admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ["message", "creation_date"]
     search_fields = ["message"]
     ordering = ["creation_date"]
 
 
-admin.site.register(Feedback, FeedbackAdmin)
+@admin.register(CategorySurveyAnswer)
+class CategorySurveyAnswerAdmin(admin.ModelAdmin):
+    list_display = ["answer", "creation_date", "category"]
+    search_fields = ["answer"]
+    ordering = ["creation_date"]
