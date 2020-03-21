83db9f5f0c98fef11bb311c4476a915dc1e46191
Neobyte01 <neobyte01@hotmail.com>
2020-03-21 16:21:02 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/management/commands/rank.py
+++ products/management/commands/rank.py
@@ -1,20 +1,18 @@
 from django.core.management.base import BaseCommand
+from django.contrib.contenttypes.models import ContentType
 import inspect
 import sys
 
 
 class Command(BaseCommand):
-    help = "Update the product rankings"
+    help = "Update specification rankings"
 
     def handle(self, *args, **kwargs):
-        for name, obj in inspect.getmembers(sys.modules[__name__]):
-            print(obj)
-            if inspect.isclass(obj):
-                print("class")
+        for content_type in ContentType.objects.all():
+            model_class = content_type.model_class()
 
-                rank = getattr(self, "rank", None)
-                if callable(rank):
-                    obj.rank()
-                    print("here")
+            rank = getattr(model_class, "rank", None)
+            if callable(rank):
+                model_class.rank()
 
         self.stdout.write(self.style.SUCCESS("Successfully updated all rankings"))
