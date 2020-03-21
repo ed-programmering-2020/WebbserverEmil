e58fcd427afdcb0eae790edd6c2e163a8b0ebcfc
Neobyte01 <neobyte01@hotmail.com>
2020-03-21 15:55:44 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/admin/specifications.py
+++ products/admin/specifications.py
@@ -1,21 +1,65 @@
+from ..models.specifications.measurements import BatteryTime, Weight
+from ..models.specifications.memory import Ram, StorageSize, StorageType
+from ..models.specifications.panel import PanelType, RefreshRate, Resolution, ScreenSize
+from ..models.specifications.processing import Processor, GraphicsCard
+
 from django.contrib import admin
-from products.models import BenchmarkSpecification, SpecifiedSpecification, StandardSpecification
 
 
 class SpecificationAdmin(admin.ModelAdmin):
     list_display = ["score", "content_type"]
 
 
-@admin.register(BenchmarkSpecification)
-class BenchmarkedSpecificationAdmin(SpecificationAdmin):
+@admin.register(BatteryTime)
+class BatteryTimeAdmin(SpecificationAdmin):
     pass
 
 
-@admin.register(SpecifiedSpecification)
-class SpecifiedSpecificationAdmin(SpecificationAdmin):
+@admin.register(Weight)
+class WeightAdmin(SpecificationAdmin):
     pass
 
 
-@admin.register(StandardSpecification)
-class StandardSpecificationAdmin(SpecificationAdmin):
+@admin.register(Ram)
+class RamAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(StorageSize)
+class StorageSizeAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(StorageType)
+class StorageTypeAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(PanelType)
+class PanelTypeAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(RefreshRate)
+class RefreshRateAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(Resolution)
+class BatteryTimeAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(ScreenSize)
+class ScreenSizeAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(Processor)
+class ProcessorAdmin(SpecificationAdmin):
+    pass
+
+
+@admin.register(GraphicsCard)
+class GraphicsCardAdmin(SpecificationAdmin):
     pass
