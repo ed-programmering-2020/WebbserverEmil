56ee3b54e5414b4d60fbd06ce9e510026115d145
Neobyte01 <neobyte01@hotmail.com>
2020-03-21 16:18:04 +0100

    Fix when metaproduct get price returns None in product combiner

--- products/management/commands/rank.py
+++ products/management/commands/rank.py
@@ -8,10 +8,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for name, obj in inspect.getmembers(sys.modules[__name__]):
+            print(obj)
             if inspect.isclass(obj):
+                print("class")
 
                 rank = getattr(self, "rank", None)
                 if callable(rank):
                     obj.rank()
+                    print("here")
 
-        self.stdout.write(self.style.SUCCESS("Successfully updated product rankings"))
+        self.stdout.write(self.style.SUCCESS("Successfully updated all rankings"))
