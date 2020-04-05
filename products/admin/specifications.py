e2daf15b1498ba0a282950658a8995de9df25fce
Neobyte01 <neobyte01@hotmail.com>
2020-04-05 21:34:50 +0200

    Fix when metaproduct get price returns None in product combiner

--- products/admin/specifications.py
+++ products/admin/specifications.py
@@ -17,13 +17,8 @@ from django.contrib import admin
 
 
 class SpecificationAdmin(admin.ModelAdmin):
-    list_display = ["raw_value", "score"]
-
-    search_fields = ["raw_value"]
-
-
-class BenchmarkedSpecificationAdmin(SpecificationAdmin):
-    list_display = SpecificationAdmin.list_display + ["full_score"]
+    list_display = ["value", "score"]
+    search_fields = ["value"]
 
 
 @admin.register(BatteryTime)
@@ -72,10 +67,10 @@ class ScreenSizeAdmin(SpecificationAdmin):
 
 
 @admin.register(Processor)
-class ProcessorAdmin(BenchmarkedSpecificationAdmin):
+class ProcessorAdmin(SpecificationAdmin):
     pass
 
 
 @admin.register(GraphicsCard)
-class GraphicsCardAdmin(BenchmarkedSpecificationAdmin):
+class GraphicsCardAdmin(SpecificationAdmin):
     pass
