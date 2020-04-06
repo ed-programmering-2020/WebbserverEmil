a666caa8df731a1d38d984c8352adc4be28de996
Neobyte01 <neobyte01@hotmail.com>
2020-04-06 20:53:10 +0200

    Fix when metaproduct get price returns None in product combiner

--- Orpose/urls.py
+++ Orpose/urls.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from .views import FrontendAppView
+import os
+
+
+def get_file_url_path(file, content_type):
+    return path(file, TemplateView.as_view(
+        template_name=os.path.join(settings.STATIC_ROOT, file),
+        content_type=content_type)
+    )
 
 
 urlpatterns = [
@@ -11,6 +20,16 @@ urlpatterns = [
     path("api/", include("content.urls")),
     path("api/auth/", include("users.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+files = [
+    ("sitemap.xml", "application/xml"),
+    ("robots.txt", "text/plain"),
+]
+for file in os.listdir(settings.STATIC_ROOT):
+    if file.endswith(".js"):
+        files.append((file, "text/javascript"))
+for file, content_type in files:
+    urlpatterns.append(get_file_url_path(file, content_type))
 
 if not getattr(settings, "DEBUG", None):
     urlpatterns.append(re_path(r'.*', FrontendAppView.as_view()))
