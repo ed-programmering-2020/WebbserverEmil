f8a95438722517182c2b4071b50d00eb3d5943b0
Neobyte01 <neobyte01@hotmail.com>
2020-04-14 12:14:53 +0200

    Include frontend build

---
+++ static/precache-manifest.00f2b1b76379fade5c3a1926fef95f6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5def59ee2a1b605e1910254a9a5e0f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07c0754218817a9d6597",
+    "url": "/static/css/2.72d5056d.chunk.css"
+  },
+  {
+    "revision": "128dc42fed60302fd7d8",
+    "url": "/static/css/main.01529597.chunk.css"
+  },
+  {
+    "revision": "07c0754218817a9d6597",
+    "url": "/static/js/2.cfd709fc.chunk.js"
+  },
+  {
+    "revision": "189ac4b42ce0077d445e0012902600e1",
+    "url": "/static/js/2.cfd709fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128dc42fed60302fd7d8",
+    "url": "/static/js/main.5d49fb2c.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  },
+  {
+    "revision": "2a7fd389f51d97393ae88fd51ee3c023",
+    "url": "/static/media/default-image.2a7fd389.jpg"
+  },
+  {
+    "revision": "548e107e167b8d026000bec2e481699f",
+    "url": "/static/media/main-page-background.548e107e.png"
+  }
+]);
