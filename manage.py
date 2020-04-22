969f6b9c0169c11ae573719d39d4b9c97058aff4
Neobyte01 <neobyte01@hotmail.com>
2020-04-22 15:05:57 +0200

    a

--- manage.py
+++ manage.py
@@ -1,9 +1,7 @@
 import sys
-import os
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Orpose.settings.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
