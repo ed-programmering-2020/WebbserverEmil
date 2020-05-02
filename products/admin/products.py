fc7e1ba3d50286f5240876732577498843f787c0
Neobyte01 <neobyte01@hotmail.com>
2020-05-02 10:34:24 +0200

    Image inline ordering

--- products/admin/products.py
+++ products/admin/products.py
@@ -19,7 +19,7 @@ class MetaProductInline(admin.TabularInline):
 class ImageInline(admin.TabularInline):
     classes = ["collapse"]
     readonly_fields = ["thumbnail", "host"]
-    ordering = ["placement", "-is_active"]
+    ordering = ["-is_active", "placement"]
     exclude = ["url"]
     model = Image
     extra = 0
