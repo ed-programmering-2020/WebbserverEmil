34cdbd874429fb7bbae272e3d20e75f5a109f590
Neobyte01 <neobyte01@hotmail.com>
2020-05-11 10:49:41 +0200

    Update

--- Orpose/urls.py
+++ Orpose/urls.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import index
 
 
 urlpatterns = [
     # API
-    path("", include("products.urls")),
-    path("", include("content.urls")),
+    path("api/", include("products.urls")),
     path("auth/", include("users.urls")),
 
     # Website
     path("", index),
-    path("clone-of-erik/", admin.site.urls),
+    path("admin/", admin.site.urls),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
