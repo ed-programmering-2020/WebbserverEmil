0e2c3b46d062f8776e68c2a7ee61658cb5031db3
Neobyte01 <neobyte01@hotmail.com>
2020-05-11 11:07:00 +0200

    Automatically choose settings file

--- Orpose/settings.py
+++ Orpose/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.sessions',
+    'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from django.core.management import execute_from_command_line
-import sys
+import sys, os
 
 
 if __name__ == '__main__':
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'Orpose.settings'
     execute_from_command_line(sys.argv)
 
