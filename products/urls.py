7d328359693ed9cf3d60038af7b625757a292280
Emil Wagman <31895118+Neobyte01@users.noreply.github.com>
2020-05-24 12:00:44 +0200

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from products.views import LaptopAPI, ScrapingAPI
 
 urlpatterns = [
-    path("laptop/<int:laptop_id>/<str:slug>", LaptopAPI.as_view()),
+    path("laptop/", LaptopAPI.as_view()),
     path("scraping/products/", ScrapingAPI.as_view()),
 ]
